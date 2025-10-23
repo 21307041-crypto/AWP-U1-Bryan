98b49b817ea50390b902a0f6a7bcdeec30bf6d83
21307041-crypto <21307041@utcgg.edu.mx>
2025-10-23 12:04:11 -0600

    Update main.js

--- AWP/U1/sw2/main.js
+++ AWP/U1/sw2/main.js
@@ -1,9 +1,11 @@
 //Registrar el service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("service-worker.js")
-        .then((reg) => {console.log("SW registrado. ", reg);
+    navigator.serviceWorker.register("./sw.js")
+        .then((reg) => {
+            console.log("SW registrado. ", reg);
         })
-        .catch((err) => { console.log("Error al registrar el sw", err);
+        .catch((err) => { 
+            console.log("Error al registrar el sw", err);
         });
 }
 
@@ -11,27 +13,27 @@ if ('serviceWorker' in navigator) {
 document.getElementById("check").addEventListener("click", () => {
     if (navigator.serviceWorker.controller) {
         alert("El SW esta activo y controlando la pagina");
-    }else{
+    } else {
         alert("El SW aun no esta activo");
     }
 });
+
 //Pedir permiso de notificaciones
 if (Notification.permission === "default") {
     Notification.requestPermission().then((perm) => {
-    if (perm === "granted") {
-        console.log("Permiso de Notificacion concedido");
-    } else {
-        console.log("Permiso de Notificacion denegado");
-    }
-});
+        if (perm === "granted") {
+            console.log("Permiso de Notificacion concedido");
+        } else {
+            console.log("Permiso de Notificacion denegado");
+        }
+    });
 }
 
 // Botón para lanzar notificación local
 document.getElementById("btnNotificacion").addEventListener("click", () => {
-    if(navigator.serviceWorker.controller){
-        navigator.serviceWorker.controller.postMessage
-        ("mostrar-notificacion");
+    if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller.postMessage("mostrar-notificacion");
     } else {
         console.log("El SW no está activo aún");
     }
-});   
+});
