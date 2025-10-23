950a1da1fa580b96d5c256b2235d5949aa18e0f4
21307041-crypto <21307041@utcgg.edu.mx>
2025-10-23 12:54:02 -0600

    Update main.js

--- AWP/U1/sw2/main.js
+++ AWP/U1/sw2/main.js
@@ -1,24 +1,27 @@
 //registrar el service worker completo
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-        .then((reg) => console.log('SW registrado', reg))
-        .catch((err) => console.log('Error al registrar el SW', err));
-}
 
-//Boton para verificar el estado del SW
-document.getElementById('check').addEventListener('click', () => {
-    if (navigator.serviceWorker.controller) {
-        alert('El SW esta activo');
-    } else {
-        alert('El SW no esta activo');
-    }
-});
+if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('service-worker.js')
+            .then((reg) => console.Log('SW registrado', reg)
+            ).catch((err) => console.Log('Error al registrar el SW', err));
+
+        }
+
+        //Boton para verificar el estado del SW
+        document.getElementById('check').addEventListener('click', () => {
+            if (navigator.serviceWorker.controller) {
+                alert('El SW esta activo');
+            } else {
+                alert('El SW no esta activo');
+            }
+        });
 
 //Pedir permiso de notificacion
 if (Notification.permission === 'default') {
     Notification.requestPermission().then((perm) => {
         if (perm === 'granted') {
-            console.log('Permiso de notificación concedido');
+console.log('Permiso de notificación concedido');
+
         } else {
             console.log('Permiso de notificación denegado');
         }
@@ -27,9 +30,12 @@ if (Notification.permission === 'default') {
 
 //Boton para lanzar notificacion local
 document.getElementById("btnNotificacion").addEventListener('click', () => {
-    if (navigator.serviceWorker.controller) {
+
+    if (navigator.serviceWorker.controller)
         navigator.serviceWorker.controller.postMessage('mostrar-notificacion');
-    } else {
+    else {
+
         console.log('El service Worker no esta activo aun');
+        
     }
 });
