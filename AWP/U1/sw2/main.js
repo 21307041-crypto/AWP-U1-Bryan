b2503dc5f519e121cd196c24397145206a5685e0
21307041-crypto <21307041@utcgg.edu.mx>
2025-10-23 11:31:44 -0600

    Update main.js

--- AWP/U1/sw2/main.js
+++ AWP/U1/sw2/main.js
@@ -1,41 +1,37 @@
-//registrar el service worker completo
-
+//Registrar el service worker
 if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('service-worker.js')
-            .then((reg) => console.Log('SW registrado', reg)
-            ).catch((err) => console.Log('Error al registrar el SW', err));
-
-        }
-
-        //Boton para verificar el estado del SW
-        document.getElementById('check').addEventListener('click', () => {
-            if (navigator.serviceWorker.controller) {
-                alert('El SW esta activo');
-            } else {
-                alert('El SW no esta activo');
-            }
+    navigator.serviceWorker.register("service-worker.js")
+        .then((reg) => {console.log("SW registrado. ", reg);
+        })
+        .catch((err) => { console.log("Error al registrar el sw", err);
         });
-
-//Pedir permiso de notificacion
-if (Notification.permission === 'default') {
-    Notification.requestPermission().then((perm) => {
-        if (perm === 'granted') {
-console.log('Permiso de notificación concedido');
-
-        } else {
-            console.log('Permiso de notificación denegado');
-        }
-    });
 }
 
-//Boton para lanzar notificacion local
-document.getElementById("btnNotificacion").addEventListener('click', () => {
-
-    if (navigator.serviceWorker.controller)
-        navigator.serviceWorker.controller.postMessage('mostrar-notificacion');
-    else {
-
-        console.log('El service Worker no esta activo aun');
-        
+//Boton para verficar el estado del SW
+document.getElementById("check").addEventListener("click", () => {
+    if (navigator.serviceWorker.controller) {
+        alert("El SW esta activo y controlando la pagina");
+    }else{
+        alert("El SW aun no esta activo");
     }
 });
+//Pedir permiso de notificaciones
+if (Notification.permission === "default") {
+    Notification.requestPermission().then((perm) => {
+    if (perm === "granted") {
+        console.log("Permiso de Notificacion concedido");
+    } else {
+        console.log("Permiso de Notificacion denegado");
+    }
+});
+}
+
+// Botón para lanzar notificación local
+document.getElementById("btnNotificacion").addEventListener("click", () => {
+    if(navigator.serviceWorker.controller){
+        navigator.serviceWorker.controller.postMessage
+        ("mostrar-notificacion");
+    } else {
+        console.log("El SW no está activo aún");
+    }
+});   
