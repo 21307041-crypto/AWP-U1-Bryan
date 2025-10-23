3f9f82ee86326ec2b9598b58cf252df834c8f861
21307041-crypto <21307041@utcgg.edu.mx>
2025-10-23 12:53:14 -0600

    Update sw.js

--- AWP/U1/sw/sw.js
+++ AWP/U1/sw/sw.js
@@ -14,10 +14,7 @@ const urlsToCache = [
 self.addEventListener("install", (event) => {
     console.log("SW: Instalado");
 
-    // Saltar waiting para activarse inmediatamente
-    self.skipWaiting();
-
-    //event.waitUntil() asegura que la instalacion espere hasta que se complete la promise() de cachear los archivos
+    //event.waitUntil() asegura que la instalacion esoere hasta que se complete la promise() de cachear los archivos
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             console.log("SW: Archivos cacheados");
@@ -35,7 +32,7 @@ self.addEventListener("activate", (event) => {
     event.waitUntil(
         //Caches.keys() obtiene todos los nombres de caches almacenados
         caches.keys().then((cacheNames) => 
-            //Promise.all() espera a que se eliminen todos los caches viejos
+            //Promises.all() espera a que se eliminen todos los caches viejos
             Promise.all(
                 cacheNames.map((cache) => {
                     //si el cache no coincide con el actual se elimina
@@ -45,30 +42,6 @@ self.addEventListener("activate", (event) => {
                     }
                 })
             )
-        ).then(() => {
-            // Tomar control de todas las pestañas inmediatamente
-            console.log("SW: Tomando control de las pestañas");
-            return self.clients.claim();
-        })
-    );
-});
-
-// Evento fetch - Interceptar peticiones (ESENCIAL que falataba)
-self.addEventListener("fetch", (event) => {
-    console.log("SW: Interceptando petición:", event.request.url);
-    
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Si está en cache, devolverlo
-                if (response) {
-                    console.log("SW: Sirviendo desde cache:", event.request.url);
-                    return response;
-                }
-                
-                // Si no está en cache, hacer fetch normal
-                console.log("SW: Haciendo petición a internet:", event.request.url);
-                return fetch(event.request);
-            })
+        )
     );
 });
