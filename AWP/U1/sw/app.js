86b6646086a765faea9b170f52659dd4b9d918d7
21307041-crypto <21307041@utcgg.edu.mx>
2025-10-22 18:55:11 -0600

    Update app.js

--- AWP/U1/sw/app.js
+++ AWP/U1/sw/app.js
@@ -1,15 +1,15 @@
 //Verificar si el navegador soporta Service Worker
 if ('serviceWorker' in navigator) {
     //Llamar el metodo register para registrar el SW
-    //El primer parametro es el archivo de Service Worker
     navigator.serviceWorker.register('./sw.js')
-        //then se ejecuta si el registro fue exitoso
-        //reg es un objeto tipo ServiceWorkerRegistration con información del sw
-        .then(reg => console.log("Service Worker registrado", reg))
-        //catch se ejecuta si hubo un error en el registro
-        //err contiene el mensaje de error o detalles del error
-        .catch((error) => console.log("Error al registrar el SW", err));
+        .then(reg => {
+            console.log("Service Worker registrado", reg);
+        })
+        .catch((error) => {
+            console.log("Error al registrar el SW", error);
+        });
 }
+
 //Agregamos un evento click al botón
 document.getElementById("check").addEventListener("click", () => {
     //Verificamos si el sw controla la pagina actual
