8bddbd3ad1cc38f568a1a8115a2380169bfb8d70
21307041-crypto <21307041@utcgg.edu.mx>
2025-10-23 12:18:36 -0600

    Update app.js

--- AWP/U1/sw/app.js
+++ AWP/U1/sw/app.js
@@ -1,34 +1,21 @@
-//verficar si el navegador soporta service worker
-if('serviceWorker' in navigator){
-
-    //llamar al meytodo register para registrar el service worker
-    //El parametro /sw.js es la ruta del archivo del service worker
-    navigator.serviceWorker.register('sw.js')
-
-    //then se ejecuta si el registro fue exitoso
-    //reg es un objrto tipo ServiceWorkerRegistration con informacion del SW
-   .then((reg) => console.log('Service Worker registrado', reg))
-    //catch se ejecuta si hubo un error al registrar el SW
-    //err contiene el mensaje o detalle del error
-    .catch((err) => console.log('Error al registrar el SW:', err));
+//Verificar si el navegador soporta Service Worker
+if ('serviceWorker' in navigator) {
+    //Llamar el metodo register para registrar el SW
+    //El primer parametro es el archivo de Service Worker
+    navigator.serviceWorker.register('./sw.js')
+        //then se ejecuta si el registro fue exitoso
+        //reg es un objeto tipo ServiceWorkerRegistration con información del sw
+        .then(reg => console.log("Service Worker registrado", reg))
+        //catch se ejecuta si hubo un error en el registro
+        //err contiene el mensaje de error o detalles del error
+        .catch((error) => console.log("Error al registrar el SW", err));
 }
-
-//Agregamos un evento clic al boton check
-document.getElementById('check').addEventListener('click', () => {
-    //verificamos si hay un service worker activo
-    if(navigator.serviceWorker.controller){ 
-        alert('El SW esta activo y controlando la pagina');
+//Agregamos un evento click al botón
+document.getElementById("check").addEventListener("click", () => {
+    //Verificamos si el sw controla la pagina actual
+    if (navigator.serviceWorker.controller) {
+        alert("El Service Worker está Activo y controlando la pagina actual");
     } else {
-        alert('El SW no esta activo');
+        alert("El Service Worker NO está activo");
     }
 });
-
-
-//Area de notificacion 
-if (Notification.permission === "default") {
-    Notification.requestPermission().then((perm) => {
-        if(perm === "granted"){
-            console.log("Permiso de notificaciones concedido");
-        }
-    });
-}
